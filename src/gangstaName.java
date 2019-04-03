857385f897e4269151344974104ea08eadf1324e
TPNxl <timmynadolsky@gmail.com>
2019-04-03 12:16:18 -0500

    dddd

--- src/gangstaName.java
+++ src/gangstaName.java
@@ -5,6 +5,7 @@ public class gangstaName {
         Scanner sc = new Scanner(System.in);
         System.out.print("Type your name, playa: ");
         String input = sc.nextLine();
+        sc.close();
         String firstName = input.substring(0, input.indexOf(" "));
         String lastName = input.substring(input.indexOf(" ")+1);
         System.out.println("Your gangsta name is \"" + firstName.charAt(0) + ". Diddy "+lastName.toUpperCase() + " " + firstName+"-izzle\"");
